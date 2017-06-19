98bb9b7487592f94b8afbe8388ae725ed3f0e9bc
wangdongsong1229 <wangdongsong1229@163.com>
2017-06-19 22:44:17 +0800

    填写注释

--- src/main/scala/com/wds/scala/base/obj/Pizza.scala
+++ src/main/scala/com/wds/scala/base/obj/Pizza.scala
@@ -2,6 +2,11 @@ package com.wds.scala.base.obj
 
 /**
   * 4.3 定义从个辅助构造函数
+  *
+  * 1、辅助构造函数必须使用this命名创建
+  * 2、辅助构造函数必须从调用之前定义的构造函数开始
+  * 3、必须有不同的签名
+  * 4、一个构造函数通过this调用另一个不同的构造函数
   * Created by wangdongsong1229@163.com on 2017/6/19.
   */
 class Pizza(var crustSize: Int, var crustType: String) {
