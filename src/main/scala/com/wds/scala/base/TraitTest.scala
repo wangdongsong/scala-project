cc786ea00abff6edcd401577cfead79f3e85becc
wangdongsong1229 <wangdongsong1229@163.com>
2017-06-25 11:21:21 +0800

    物质作为接口使用 继承类中使用特质的抽象字段和实际字段

---
+++ src/main/scala/com/wds/scala/base/TraitTest.scala
@@ -0,0 +1,35 @@
+package com.wds.scala.base
+
+/**
+  * 8 特质
+  * train物质大多数情况下类似于Java的接口，Scala中可以继承多个特质，使用extends和with关键字
+  * Created by wangdongsong1229@163.com on 2017/6/25.
+  */
+object TraitTest {
+  def main(args: Array[String]): Unit = {
+
+  }
+}
+
+/**
+  * 8.1 特质作为接口使用
+  */
+trait Computer{
+  var size = 14
+  val maxNumToppings = 10
+  var outputSize: Int //abstract
+
+  def play
+  def input
+}
+
+class MyComputer extends Computer{
+
+  //8.2 使用特质中的抽象字段和实际字段
+  size = 13
+  var outputSize = 5
+
+  override def play: Unit = println("play")
+
+  override def input: Unit = println("input")
+}
