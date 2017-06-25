2aa2beb8730268b2e28f3faed20854639b49e7dc
wangdongsong1229 <wangdongsong1229@163.com>
2017-06-25 12:20:53 +0800

    简单混入特质

--- src/main/scala/com/wds/scala/base/TraitTest.scala
+++ src/main/scala/com/wds/scala/base/TraitTest.scala
@@ -24,6 +24,42 @@ trait Computer{
   def input
 }
 
+/**
+  * 3.3 特质作为抽象类使用
+  */
+trait Phone{
+  def speak {println("phone")}
+  def signal
+}
+
+trait PetTail{
+  def wagTail{println("tail is wagging")}
+  def stopTail{println("tail is stopped")}
+}
+
+abstract class Pet(var name: String){
+  def spack //abstract
+  def ownerIsHome{println("excited")}
+  def jumpForJoy{println("jumping for joy")}
+}
+
+/**
+  * 8.4 简单混入特质
+  * @param name
+  */
+class PetDog(name: String) extends Pet(name) with PetTail{
+  override def spack: Unit = println("PetDog")
+
+  override def ownerIsHome: Unit = {
+    wagTail
+    spack
+  }
+}
+
+class IPhone extends Phone{
+  override def signal: Unit = println("IPhone")
+}
+
 class MyComputer extends Computer{
 
   //8.2 使用特质中的抽象字段和实际字段
