3d600f65fc4343eba5b1e0cbce39b972e1386cd7
wangdongsong1229 <wangdongsong1229@163.com>
2017-07-08 11:01:19 +0800

    处理文件中的字符

--- src/main/scala/com/wds/scala/base/file/OpenReadFile.scala
+++ src/main/scala/com/wds/scala/base/file/OpenReadFile.scala
@@ -1,6 +1,6 @@
 package com.wds.scala.base.file
 
-import java.io.{File, PrintWriter}
+import java.io._
 
 import scala.io.Source
 
@@ -11,7 +11,68 @@ object OpenReadFile {
 
   def main(args: Array[String]): Unit = {
     //openFile
-    writeFile
+    //writeFile
+    //rwBinaryFile
+    processChar
+  }
+
+  /**
+    * 12.4 如何处理文本文件中的每个字符
+    */
+  def processChar: Unit ={
+
+    val fileName = "E:\\javaTest\\scala.txt"
+
+    //不考虑性能
+    val source = Source.fromFile(fileName)
+    for(char <- source){
+      println(char.toUpper)
+    }
+    source.close()
+
+    //统计行数，不考虑性能
+    val NEWLine = 10
+    var newLineCount = 0L
+    for{
+      char <- source
+      if char.toByte == NEWLine
+    } newLineCount += 1
+    println(newLineCount)
+
+    //性能好的
+    var performaceCount = 0L
+    for{
+      line <- source.getLines()
+      c <- line
+      if c.toByte == NEWLine
+    } performaceCount += 1
+    println(performaceCount)
+  }
+
+  /**
+    * 12.3 读写二制文件
+    */
+  def rwBinaryFile: Unit = {
+    var in = None: Option[FileInputStream]
+    var out = None: Option[FileOutputStream]
+
+    try {
+      in = Some(new FileInputStream("E:\\javaTest\\scala.txt"))
+      out = Some(new FileOutputStream("E:\\javaTest\\scalaBinary.txt"))
+      var c = 0
+      while ( {
+        c = in.get.read; c != -1
+      }) {
+        out.get.write(c)
+      }
+    } catch {
+      case e: IOException => e.printStackTrace
+    }finally {
+      println("finish")
+      if(in.isDefined) in.get.close()
+      if(out.isDefined) out.get.close()
+    }
+
   }
 
   /**
