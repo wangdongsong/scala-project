81ebba2e1bc329b492fa7d0a3004a705a4a64bf9
wangdongsong1229 <wangdongsong1229@163.com>
2017-07-10 06:57:52 +0800

    ping pong Actor传递消息

--- src/main/scala/com/wds/scala/base/actor/HelloActor.scala
+++ src/main/scala/com/wds/scala/base/actor/HelloActor.scala
@@ -2,7 +2,7 @@ package com.wds.scala.base.actor
 
 import java.util.concurrent.TimeUnit
 
-import akka.actor.{Actor, ActorSystem, Props, Terminated}
+import akka.actor.{Actor, ActorRef, ActorSystem, Props, Terminated}
 
 import scala.concurrent.duration.Duration
 import scala.concurrent.{Await, Future}
@@ -18,6 +18,10 @@ class HelloActor extends Actor{
   }
 }
 
+/**
+  * 13.2 带有参数的Actor
+  * @param name
+  */
 class ParamHelloActor(name: String) extends Actor{
   override def receive: Receive = {
     case "hello" => println(s"hello back to you, $name")
@@ -25,16 +29,64 @@ class ParamHelloActor(name: String) extends Actor{
   }
 }
 
-object Main extends App{
-  val system = ActorSystem("HelloSystem")
+class Ping(pong: ActorRef) extends Actor{
+  var count = 0
+  def incrementAndPrint: Unit ={
+    count += 1
+    println("ping")
+  }
 
-  val helloActor = system.actorOf(Props[HelloActor], name = "helloactor")
+  override def receive: Receive = {
+    case "start" => {
+      incrementAndPrint
+      pong ! "pong"
+    }
+    case "ping" => {
+      incrementAndPrint
+      if (count > 99) {
+        sender ! "stop"
+        println("ping stoped")
+        context.stop(self)
+      }else{
+        sender ! "pong"
+      }
+    }
+    case _ => println("Ping got something unexpected")
+  }
+}
 
-  helloActor ! "hello"
-  helloActor ! "buenos dias"
+class Pong extends Actor{
+  override def receive: Receive = {
+    case "pong" => {
+      println("pong")
+      sender ! "ping"
+    }
+    case "stop" => {
+      println("pong stoped")
+      context.stop(self)
+    }
+    case _ => println("Pong got something unexpected.")
+  }
+}
 
-  val paramHelloActor = system.actorOf(Props(new ParamHelloActor("Fred")), name = "paramHelloActor")
-  paramHelloActor ! "hello"
+object Main {
 
-  Await.ready(system.whenTerminated, Duration(1, TimeUnit.MINUTES))
+  def main(args: Array[String]): Unit = {
+    val system = ActorSystem("HelloSystem")
+
+    helloActor(system)
+
+    system.terminate()
+  }
+
+
+  private def helloActor(system: ActorSystem) = {
+    val helloActor = system.actorOf(Props[HelloActor], name = "helloactor")
+
+    helloActor ! "hello"
+    helloActor ! "buenos dias"
+
+    val paramHelloActor = system.actorOf(Props(new ParamHelloActor("Fred")), name = "paramHelloActor")
+    paramHelloActor ! "hello"
+  }
 }
